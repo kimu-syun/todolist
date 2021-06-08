5173c055b7c41f1dbf46630961d1399c252a89e1
kimu-syun <0108kimusyun@gmail.com>
2021-06-09 06:32:09 +0900

    todolist

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,19 +3,22 @@
     <h1>{{ msg }}</h1>
     <counter />
     <item-list/>
+    <todolist/>
   </div>
 </template>
 
 <script>
 import Counter from "./Counter.vue";
 import ItemList from "./ItemList.vue";
+import Todolist from './Todolist.vue';
 
 export default {
   components: {
     Counter,
     ItemList,
+    Todolist,
   },
-  props:{
+  props: {
     msg: {
       type: String,
       default: "",
---
+++ src/components/Todolist.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div>TodoList</div>
+    <div>
+      <label>
+        Task
+        <input v-model="NewTasksname" type="text" />
+      </label>
+      <lebel>
+          Limit
+          <input v-model="Limittime" type="text" />
+      </lebel>
+      <button @click="addTask">add</button>
+    </div>
+    <div v-for="task in tasks" :key="task.name">
+        <lebel v-bind:class="{ done: task.isChecked}">
+            <input type="checkbox" v-model="task.isChecked"> {{task.name}}　期限：{{task.time}}
+        </lebel>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const tasks = ref([
+    ]);
+    const NewTasksname = ref("");
+    const Limittime = ref("")
+
+    const addTask = () => {
+        if (NewTasksname.value != "") {
+         tasks.value.push({ name: NewTasksname.value, time: Limittime.value, isChecked: false});
+        }
+        NewTasksname.value = "";
+        Limittime.value = "";
+    };
+
+    return { tasks, NewTasksname, Limittime, addTask };
+  },
+
+};
+</script>
+
+<style>
+.done { text-decoration: line-through;}
+</style>
